ea22bc7be0da974471f5801540094f6c59b247cd
Nathália Pereira <nathalia.pereira252000@gmail.com>
2022-03-27 02:03:12 -0300

    Adiciona link para redirecionar entre as páginas de signin e signup

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import {
     Container,
     Form,
-    Title
+    Title,
 } from './styles'
 import api from '../../services/api';
 import Input from '../../components/Input';
@@ -44,6 +44,10 @@ const SignIn = () => {
         });
     }
 
+    const handleRedirect = () => {
+        navigate('/signup')
+    }
+
     return (
         <>
         <Container>
@@ -52,6 +56,7 @@ const SignIn = () => {
                 <Input type = 'email' value={ email } onChange={ setEmail } placeholder='Email' />
                 <Input type = 'password' value={ password } onChange={ setPassword } placeholder='Password' />
                 <Button>Send</Button>
+                <span>Ainda não tem uma conta? <Link to='signup'>Cadastre-se</Link></span>
             </Form>
         </Container>
         </>
